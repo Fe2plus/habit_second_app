d9604c390fcbb459840ef22fddc5b45726f4c4e5
Fe2plus <unspeakablefe@gmail.com>
2018-03-02 19:34:01 +0900

    Add gem_twitter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:habit)
+    params.require(:post).permit(:habit, :friend)
   end
 end
