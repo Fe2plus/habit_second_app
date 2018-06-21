7488212f74eeae78652db9f196ae2922321e5a33
Fe2plus <unspeakablefe@gmail.com>
2018-06-22 00:54:41 +0900

    tweeter_follower

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,31 @@
 module UsersHelper
+  #FRIENDS_COUNT = 10.freeze
+=begin
+  def get_follower
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_KEY']
+      config.consumer_secret = ENV['TWITTER_SECRET']
+      # ユーザー情報の設定
+      config.access_token = @current_user.twitter_token
+      config.access_token_secret  = @current_user.twitter_secret
+    end
+    screen_names = client.friends.collect {|f| client.user(f).screen_name }
+    five_names = screen_names.take(1)
+    puts (five_names)
+  end
+=end
+
+=begin
+  def get_follower
+    @twitter ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["CONSUMER_KEY"]
+      config.consumer_secret = ENV["CONSUMER_SECRET"]
+      config.access_token = @current_user.twitter_token
+      config.access_token_secret = @current_user.twitter_secret
+    end
+    response = @twitter.friends(cursor: params[:cursor], count: FRIENDS_COUNT)
+    @next = response.attrs[:next_cursor]
+    @friends = response.take(FRIENDS_COUNT)
+  end
+=end
 end
